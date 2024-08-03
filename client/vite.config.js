b9de18fd4a62d7a7c6c2ade1a84ae966e111a3d7
AmanRawat94 <ar981089@gmail.com>
2024-08-03 18:52:07 +0530

    third update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,6 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   server: {
-//     proxy: {
-//       "/api": {
-//         target: "http://localhost:3000",
-//         secure: false,
-//       },
-//     },
-//   },
-//   plugins: [react()],
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
@@ -28,20 +11,4 @@ export default defineConfig({
     },
   },
   plugins: [react()],
-  // esbuild: {
-  //   jsxInject: `import React from 'react'`,
-  // },
-  optimizeDeps: {
-    esbuildOptions: {
-      jsx: "transform",
-      jsxFactory: "React.createElement",
-      jsxFragment: "React.Fragment",
-    },
-  },
-  json: {
-    stringify: true,
-  },
-  build: {
-    target: "esnext",
-  },
 });
